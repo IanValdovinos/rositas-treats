e817ac132ea3d85b6c0265ece99ffcc1501c2f4d
Igri04 <ifishta@ltu.edu>
2024-11-14 23:04:11 -0500

    Igri's Colombian Cocina updates

--- src/components/Kitchen/colombianCocina/index.js
+++ src/components/Kitchen/colombianCocina/index.js
@@ -5,6 +5,7 @@ import styles from './colombianCocina.module.css';
 
 // Import components
 import BaseButton from '../../Elements/BaseButton';
+import ParagraphOne from '../../Elements/ParagraphOne';
 
 // Images and Videos
 import CocinaPicture from '../../../assets/images/test/food_7.jpg';
@@ -14,19 +15,25 @@ function ColombianCocina() {
     return (
         <div>
             <div className={styles.colombianCocina}>
+
+
                 <div className={styles.colombianCocinaSection}>
                     <img src={CocinaPicture} alt='Colombian Cocina' />
                 </div>
 
                 <div className={styles.colombianCocinaItem}>
                     <h1>Colombian Cocina</h1>
-                    <p>Rosandys' love of Cartagena food is obvious in her hand made treats. Her empanadas are sure to make you miss home after eating these treats. She learned how to make the empanadas from her mother Aida while growing up in Cartagena.Rosandys started making empanada's while she was working as a professional singer in China many years ago. She had a large following of local and ex-pats that loved and missed their food from
-                    </p>
+                    <ParagraphOne>Rosandys' love of Cartagena food is obvious in her hand made treats. Her empanadas are sure to make you miss home after eating these treats. She learned how to make the empanadas from her mother Aida while growing up in Cartagena.Rosandys started making empanada's while she was working as a professional singer in China many years ago. She had a large following of local and ex-pats that loved and missed their food from
+                    </ParagraphOne>
+
+                    <div className={styles.buttonContainer}>
+                        <BaseButton additionalClassNames={styles.bannerButton}>ORDER NOW</BaseButton>
+                    </div>
                 </div>
 
             </div>
 
-            <BaseButton />
+
         </div>
 
     );
